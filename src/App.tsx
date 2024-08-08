d9cb468d5b021cd5cbb2db688aa5de6f8c41cb14
andres <anagudelogu@gmail.com>
2024-08-08 06:13:56 -0500

    Install daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import { Button } from 'react-daisyui';
 
 function App() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline'>Hello Caipirinha</h1>
+      <Button color='primary'>Click me!</Button>
       <Outlet />
     </div>
   );
